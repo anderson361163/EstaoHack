99204c65f1e556c8e3cf6522f695b807e4e6a357
EH236 <anderson361163@gmail.com>
2020-02-17 21:36:43 -0300

    gravação de dados simples v2

--- app/src/main/java/br/com/vitor/estaohack/MainActivity.kt
+++ app/src/main/java/br/com/vitor/estaohack/MainActivity.kt
@@ -1,12 +1,22 @@
 package br.com.vitor.estaohack
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val minhaPreferencia = getSharedPreferences("cadastro",Context.MODE_PRIVATE)
+
+        txvEmail.text = minhaPreferencia.getString("email","erro no email")
+        txvNome.text = minhaPreferencia.getString("email","erro no nome")
+        txvSexo.text = minhaPreferencia.getString("email", "erro no sexo")
+
+
     }
 }
